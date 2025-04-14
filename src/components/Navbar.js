f72dcf5156b0bd9e0605f5c8e78d3d0e7545ef16
Bulela <bulelagomoshe1@gmail.com>
2025-04-14 16:20:06 +0200

    logo home link update after deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,8 +15,8 @@ const MyNavbar = () => {
             
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#timeline">Timeline</Nav.Link>
-            {/* <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link> */}
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
